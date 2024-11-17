9e1ee28f7b8fee6ac08e0e980d45a82c2b67ca69
ajmalali10 <officialajmalali10@gmail.com>
2024-11-17 12:31:29 +0530

    added metadata for services

--- src/app/services/layout.tsx
+++ src/app/services/layout.tsx
@@ -1,11 +1,20 @@
 import ServiceContact from "./_components/ServiceContact";
 import ServiceNavbar from "./_components/ServiceNavbar";
+import { constructMetadata } from '@/lib/utils';
 
+
+export const metadata = constructMetadata(
+  {
+    title: "Ajmal Ali | Innovative Digital Solutions",
+    description: "Explore cutting-edge services that transform your ideas into impactful digital experiences, tailored to elevate your brand and engage your audience."
+  }
+)
 export default function ServiceLayout({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
+
     return (
         <>
          <ServiceNavbar />
