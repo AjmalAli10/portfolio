5e45176043910c83d796d4e1def0cebbef2b55f7
ajmalali10 <officialajmalali10@gmail.com>
2024-11-18 22:44:42 +0530

    added metadata in services

--- src/app/services/layout.tsx
+++ src/app/services/layout.tsx
@@ -5,7 +5,7 @@ import { constructMetadata } from '@/lib/utils';
 
 export const metadata = constructMetadata(
   {
-    title: "Ajmal Ali | Innovative Digital Solutions",
+    title: "Ajmal Ali | Innovative Digital Solutions | Freelancer | Digital Services",
     description: "Explore cutting-edge services that transform your ideas into impactful digital experiences, tailored to elevate your brand and engage your audience."
   }
 )
