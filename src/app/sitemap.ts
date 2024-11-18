4a9bf8fff6b8d309eb55f58b6940ae70a11efade
ajmalali10 <officialajmalali10@gmail.com>
2024-11-18 17:17:46 +0530

    added sitemap and robots rxt

---
+++ src/app/sitemap.ts
@@ -0,0 +1,33 @@
+import type { MetadataRoute } from "next";
+
+type ChangeFrequency =
+  | "yearly"
+  | "monthly"
+  | "always"
+  | "hourly"
+  | "daily"
+  | "weekly"
+  | "never";
+// Helper function to create a sitemap entry
+const createSitemapEntry = (
+  url: string,
+  changeFrequency: ChangeFrequency,
+  priority: number
+) => ({
+  url,
+  lastModified: new Date(),
+  changeFrequency,
+  priority,
+});
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    createSitemapEntry("https://ajmalali.me", "yearly", 1),
+    createSitemapEntry("https://ajmalali.me/services", "monthly", 0.8),
+    createSitemapEntry("https://ajmalali.me/#/about", "monthly", 0.5),
+    createSitemapEntry("https://ajmalali.me/#/projects", "monthly", 0.5),
+    createSitemapEntry("https://ajmalali.me/#/experience", "monthly", 0.5),
+    createSitemapEntry("https://ajmalali.me/#/skills", "monthly", 0.5),
+    createSitemapEntry("https://ajmalali.me/#/contact", "monthly", 0.5),
+  ];
+}
